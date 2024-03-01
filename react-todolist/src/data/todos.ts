45d73cb0c4a1e484fb87ce966dbed0c1bae24e8f
Sung Kim <sungkim7422@gmail.com>
2024-03-01 01:25:28 -0500

    added test dummy todo list

---
+++ react-todolist/src/data/todos.ts
@@ -0,0 +1,17 @@
+export const dummData = [
+    {
+        id: 1,
+        title: "Buy pizza ingredients 🍕",
+        completed: false,
+    },
+    {
+        id: 2,
+        title: "Call mum 🤦‍♀️",
+        completed: false,
+    },
+    {
+        id: 3,
+        title: "do the laundries 👕",
+        completed: false,
+    },
+];
