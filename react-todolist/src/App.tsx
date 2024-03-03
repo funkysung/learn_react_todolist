d9df4e0e91ec1789e9d578b643838e29890f347e
Sung Kim <sungkim7422@gmail.com>
2024-03-02 20:56:33 -0500

    added a useEffect and parameter to store todolist in local storage

--- react-todolist/src/App.tsx
+++ react-todolist/src/App.tsx
@@ -1,12 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 //import TodoItem from "./components/TodoItem"
 import { dummyData } from "./data/todos"
 import AddTodoForm from "./components/AddTodoForm";
 import TodoList from "./components/TodoList";
 import TodoSummary from "./components/TodoSummary";
+import { Todo } from "./types/todo";
 
 function App() {
-  const [todos, setTodos] = useState(dummyData)
+  const [todos, setTodos] = useState(() => {
+    //below section brings saved todo list from local storage (if exists)
+    const savedTodos: Todo[] = JSON.parse(localStorage.getItem("todos") || "[]")
+    return savedTodos.length > 0 ? savedTodos : dummyData;
+  })
+
+  //useEffect is a react component that does not need to be rendered every time by refresing
+  //if any paramenter is passed onto the empty array, the effect will occur only when the array value is altered
+  //this useEffect below allows to store the todolist in browser local memory every time todos item is altered
+  //can check this in work by looking in dev mode -> application -> local storage
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos])
 
 
   function setTodoCompleted(id: number, completed: boolean) {
